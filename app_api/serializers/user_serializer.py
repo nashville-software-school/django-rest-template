a9f72d3c9ab18d012dc6988d316bb0189f739cf6
Jajuan Jones <jajuanojones2@gmail.com>
2022-06-08 11:00:27 -0500

    added docstring to user serializer

--- app_api/serializers/user_serializer.py
+++ app_api/serializers/user_serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.Serializer):
+    """This class will serialize data for users"""
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name', 'username', 'email')
