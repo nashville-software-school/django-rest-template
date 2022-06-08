8a8f9cbb81bca4a4f7bc103138e8362c4ee5d6bc
Jajuan Jones <jajuanojones2@gmail.com>
2022-06-08 14:14:01 -0500

    ignore commit

--- app_api/views/auth.py
+++ app_api/views/auth.py
@@ -49,7 +49,6 @@ def register_user(request):
     )
 
     # TODO: If you're using a model with a 1 to 1 relationship to the django user, create that object here
-
     
     token = Token.objects.create(user=new_user)
     # TODO: If you need to send the client more information update the data dict
