91e3431c22278228f88cfb93bd4b7b3bfe80d7c6
Jajuan Jones <jajuanojones2@gmail.com>
2022-06-08 10:31:39 -0500

    added some changes to the file and image fields in music and profile models

--- app_api/models/music.py
+++ app_api/models/music.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Music(models.Model):
     """This class will initialize a music model"""
-    song = models.FileField()
+    song = models.FileField(upload_to='usermusic', max_length=None, null=True)
     profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
     genres = models.ManyToManyField("Genre", related_name="music")
--- app_api/models/profile.py
+++ app_api/models/profile.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     """This class will initialize a profile model"""
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    profile_img = models.FileField()
+    profile_img = models.ImageField(upload_to='profileimages', height_field=None,
+        width_field=None, max_length=None, null=True)
     tags = models.ManyToManyField("Tag", related_name="profile")
     bio = models.CharField(max_length=250)
     liked_comments = models.ManyToManyField("Comment", related_name="profile")
