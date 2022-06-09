a1cb8f3bcb374607b046d19a13741086020fb3d8
Jajuan Jones <jajuanojones2@gmail.com>
2022-06-09 11:53:57 -0500

    added category, genre, and tag views

---
+++ app_api/views/category.py
@@ -0,0 +1,23 @@
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
+from app_api.models import Category
+from app_api.serializers import CategorySerializer
+
+class CategoryView(ViewSet):
+    """This view will handle methods for data of categories"""
+    def retrieve(self, pk):
+        """Get a single category"""
+        try:
+            category = Category.objects.get(pk=pk)
+            serializer = CategorySerializer(category)
+            return Response(serializer.data)
+        except Category.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+    def list(self, request):
+        """Get all categories"""
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories)
+        return Response(serializer.data)
