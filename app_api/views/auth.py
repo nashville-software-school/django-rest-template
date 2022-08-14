98385983396ed6c57b747979137fc9e33e51c18e
Hannah Hall <hannah.hall@nashvillesoftwareschool.com>
2022-08-14 17:09:48 -0500

    add user id

--- app_api/views/auth.py
+++ app_api/views/auth.py
@@ -25,7 +25,8 @@ def login_user(request):
         # TODO: If you need to return more information to the client, update the data dict
         data = {
             'valid': True,
-            'token': token.key
+            'token': token.key,
+            'user_id': authenticated_user.id
         }
     else:
         data = { 'valid': False }
@@ -52,5 +53,5 @@ def register_user(request):
     token = Token.objects.create(user=new_user)
     # TODO: If you need to send the client more information update the data dict
     
-    data = { 'token': token.key }
+    data = { 'token': token.key, 'user_id': new_user.id }
     return Response(data, status=status.HTTP_201_CREATED)
